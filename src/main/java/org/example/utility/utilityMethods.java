dcbce32c254ca615d6f9a88cd38add2de3f7574a
Dmitry Morozov <windeks2017@gmail.com>
2021-12-12 23:37:55 +0200

    added methods to implement

--- src/main/java/org/example/utility/utilityMethods.java
+++ src/main/java/org/example/utility/utilityMethods.java
@@ -7,4 +7,12 @@ public final class utilityMethods {
     public static int add(int a, int b){
         return a + b;
     }
+    public static int subtract(int a, int b){
+        // TODO
+        return 0;
+    }
+    public static int multiply(int a, int b){
+        // TODO
+        return 0;
+    }
 }
